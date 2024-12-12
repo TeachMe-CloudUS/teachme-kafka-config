553d6403a48d8f6957adc381e296b0a209d76f25
Rami Nakkar <rami.nakkar00@gmail.com>
2024-12-12 19:37:48 +0100

    fix to use deserializer for consumption

--- src/main/java/us/cloud/teachme/kafkaconfig/config/KafkaConsumerConfig.java
+++ src/main/java/us/cloud/teachme/kafkaconfig/config/KafkaConsumerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,7 +37,7 @@ public class KafkaConsumerConfig {
                 StringDeserializer.class);
         props.put(
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
+                JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
